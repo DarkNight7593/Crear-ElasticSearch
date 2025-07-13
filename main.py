d316482ee370b11864ee705e7f5f59d266278d1f
DarkNight7593 <juan.inca@utec.edu.pe>
2025-07-13 02:51:44 -0500

    tiempo

--- main.py
+++ main.py
@@ -50,7 +50,7 @@ def crear_tenant(req: TenantRequest):
         ], check=True)
 
         # Esperar a que el servicio de Elasticsearch esté disponible
-        for intento in range(10):
+        for intento in range(20):
             try:
                 res = requests.get(f"http://localhost:{puerto}")
                 if res.status_code == 200:
